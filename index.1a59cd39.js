f279a0f66ad02d831afb361c0a7a5fe0332d9e18
YuraFLex <105123887+YuraFLex@users.noreply.github.com>
2022-07-29 17:42:18 +0000

    Deploying to gh-pages from @ YuraFLex/suspectors@dcc764ea6a2bf197226d5d464c1f9f0df4475e96 🚀

---
+++ index.1a59cd39.js
@@ -0,0 +1,2 @@
+!function(){var e,t,n;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),n=document.querySelector("body"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-hidden"),n.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.1a59cd39.js.map
