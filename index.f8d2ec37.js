c90bcdd9ec4a1db764afe8b69fd9de87b54b49ef
YuraFLex <105123887+YuraFLex@users.noreply.github.com>
2022-07-31 18:59:32 +0000

    Deploying to gh-pages from @ YuraFLex/suspectors@f92835d4740c57cc0158db88dbbd5c5b2a5a2ca4 🚀

---
+++ index.f8d2ec37.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({slidesToShow:6,asNavFor:".sliderbig"}),$(".sliderbig").slick({arrows:!1,asNavFor:".slider",slidesToShow:1})}));
+//# sourceMappingURL=index.f8d2ec37.js.map
