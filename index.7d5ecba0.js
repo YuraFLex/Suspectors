1fcf36911f85b88e4021750dfa81d35e599719ae
YuraFLex <105123887+YuraFLex@users.noreply.github.com>
2022-08-01 09:59:48 +0000

    Deploying to gh-pages from @ YuraFLex/suspectors@fa152e189b6716a0e03f6dfca27a5fe89d418ce8 🚀

--- index.7d5ecba0.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),o=document.querySelector("body");e.addEventListener("click",(()=>{const s="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!s),t.classList.toggle("is-hidden"),o.classList.toggle("no-scroll")}))})();
-//# sourceMappingURL=index.7d5ecba0.js.map
